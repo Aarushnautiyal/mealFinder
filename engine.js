eea1b49d726c381fe8ea74dfafa328299ad67e00
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-12-28 20:53:10 +0530

    the Message3

--- engine.js
+++ engine.js
@@ -2,33 +2,59 @@ const resEl = document.querySelector(".result");
 const submitEl = document.querySelector("#submit");
 const notEl = document.querySelector('.textBad')
 const dialog = document.querySelector('#dialogue')
-const form = document.querySelector("form");
-
-form.setPre
 
 
 submitEl.addEventListener('click',(e)=>{
   e.preventDefault()
-  resEl.innerHTML = "";
+    const inptEl = document.querySelector("#input").value;
+  if (inptEl<1) {
+    alert("C'mon don't be lazy Type something first")
+  } else {
+    resEl.innerHTML = "";
     notEl.innerHTML = `<strong>Nothing Found try something Else</strong>`;
-  const inptEl = document.querySelector("#input").value;
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inptEl}`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    console.log(data.meals.length);
-    notEl.innerHTML = ""
-          dialog.innerText = `Your search result for "${inptEl}"`
-           for (let i = 0; i < data.meals.length; i++) {
-             resEl.innerHTML += `<div class="card">
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inptEl}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.meals.length);
+        notEl.innerHTML = "";
+        dialog.innerText = `Your search result for "${inptEl}"`;
+        for (let i = 0; i < data.meals.length; i++) {
+          resEl.innerHTML += `<div class="card">
                 <img src="${data.meals[i].strMealThumb}" alt="">
                 <div class="text">
                     <p class="abs">${data.meals[i].strMeal}</p>
                 </div>
             </div>`;
-           }
-    
-  })
+        }
+      });
+  }
+})
+
+const random = document.querySelector('.random')
+random.addEventListener('click',(e)=>{
+  e.preventDefault()
+  const str = 'abcdefghijklmnopqrstuvwxyz';
+  const arr = str.split('')
+  let value = Math.floor(Math.random()*arr.length)
+  resEl.innerHTML = "";
+  notEl.innerHTML = `<strong>Nothing Found try something Else</strong>`;
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${arr[value]}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.meals.length);
+      notEl.innerHTML = "";
+      dialog.innerText = `Your search result for "Random Food"`;
+      for (let i = 0; i < data.meals.length; i++) {
+        resEl.innerHTML += `<div class="card">
+                <img src="${data.meals[i].strMealThumb}" alt="">
+                <div class="text">
+                    <p class="abs">${data.meals[i].strMeal}</p>
+                </div>
+            </div>`;
+      }
+    });
 })
 //   .then((data) => console.log(data.meals[0].strMeal));
 
